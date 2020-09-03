7f0fe22e2ed444895f09f69ae9cccc3890423dfd
leonardo silva <leonardohsilva.dev@gmail.com>
2020-09-02 23:51:24 -0300

    StoryBook config and writing first stories

---
+++ client/.storybook/preview.js
@@ -0,0 +1,9 @@
+import { addDecorator } from '@storybook/react'
+import withGlobalStyles from './withGlobalStyles'
+
+addDecorator(withGlobalStyles);
+
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+}
--- client/src/components/Main/index.tsx
+++ client/src/components/Main/index.tsx
@@ -1,16 +1,17 @@
 import * as S from './styles'
 // import { Wrapper } from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'React Avançado',
+  description = 'TypeScript, ReactJS, NextJS and Styled Components'
+}) => (
   <S.Wrapper>
     <S.Logo
       src="/img/logo.svg"
       alt="Imagem de um átomo e React Avançado escrito ao lado."
     />
-    <S.Title>React Avançado</S.Title>
-    <S.Description>
-      TypeScript, ReactJS, NextJS and Styled Components
-    </S.Description>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
     <S.Illustration
       src="img/hero-illustration.svg"
       alt="Imagem de um desenvolvedor de frente para uma tela com código."
