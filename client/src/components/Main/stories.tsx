2ca45a162803ed164464b910d6249db28b3c1428
leonardo silva <leonardohsilva.dev@gmail.com>
2020-09-03 23:53:41 -0300

    adjusting storybook to use essentials/controls

--- client/src/components/Main/stories.tsx
+++ client/src/components/Main/stories.tsx
@@ -1,19 +1,19 @@
 import { Meta, Story } from '@storybook/react/types-6-0'
-import { withKnobs, text } from '@storybook/addon-knobs'
 import Main from '.'
 
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs]
+  args: {
+    title: 'Title Basic',
+    description: 'Description Basic'
+  }
 } as Meta
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NextJS e Styled Components'
-    )}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
+
+export const Default: Story = (args) => <Main {...args} />
+Default.args = {
+  title: 'Title Default',
+  description: 'Description Default'
+}
